86f8d3e19d63287b40b6daec4048b9c89e2b8498
KernelErr0r <villainkernelerror@gmail.com>
2021-05-28 14:16:38 +0200

    Create very simple TCP proxy

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+const sourceAddress = "127.0.0.1:25565"
+const targetAddress = "127.0.0.1:25566"
 
 func main() {
 	fmt.Println("Hello guard-shield")
+
+	listener, err := net.Listen("tcp", sourceAddress)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Listening")
+
+	for {
+		connection, err := listener.Accept()
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Accepted connection")
+
+		go handleClient(connection)
+	}
+}
+
+func handleClient(clientConnection net.Conn) {
+	defer clientConnection.Close()
+
+	serverConnection, err := net.Dial("tcp", targetAddress)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer serverConnection.Close()
+
+	go io.Copy(clientConnection, serverConnection)
+	io.Copy(serverConnection, clientConnection)
 }
